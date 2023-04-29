2ebe6a07934ff87be3cafe69b32677b67930e496
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-04-29 13:19:24 +0200

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -1,124 +1,52 @@
-#include <iostream>
 #include "freeglut.h"
-#include "Ajedrez.h"
-#include "Casilla.h"
+#include "Coordenada.h"
+#include "Tablero.h"
 
-using namespace std;
+const int TAM_CASILLA = 75;
 
-Ajedrez ajedrez;
+Tablero tablero;
 
-//los callback, funciones que seran llamadas automaticamente por la glut
-//cuando sucedan eventos
-//NO HACE FALTA LLAMARLAS EXPLICITAMENTE
+void display() {
+	
+    glClear(GL_COLOR_BUFFER_BIT);
+    // Borde del tablero
+    glColor3f(0.4f, 0.2f, 0.0f);
+    glRecti(-75, -75, TAM_CASILLA * 8 + 75, TAM_CASILLA * 8 + 75);
 
-void OnDraw(void); //esta funcion sera llamada para dibujar
-void OnTimer(int value); //esta funcion sera llamada cuando transcurra una temporizacion
-void OnKeyboardDown(unsigned char key, int x, int y); //cuando se pulse una tecla	
-void ControlRaton(int button, int state, int x, int y);
-void createMenus();
+    // Tablero
+    for (int fila = 1; fila < TAM_TABLERO; fila++) {
 
-//GraficosAjedrez graficosAjedrez;
+        for (int columna = 1; columna < TAM_TABLERO; columna++) {
 
-int main(int argc, char* argv[])
-{
-	//Inicializar el gestor de ventanas GLUT
-	//y crear la ventana
-	glutInit(&argc, argv);
-	glutInitWindowSize(800, 600);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-	glutCreateWindow("JAQUE AL DESCUBIERTO");
+            if ((fila + columna) % 2 == 0) {
+                glColor3f(0.8f, 0.6f, 0.4f);
+            }
+            else {
+                glColor3f(0.6f, 0.4f, 0.2f);
+            }
 
-	//habilitar luces y definir perspectiva
-	glEnable(GL_LIGHT0);
-	glEnable(GL_LIGHTING);
-	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_COLOR_MATERIAL);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective( 40.0, 800/600.0f, 0.1, 150);
+            glRecti(columna * TAM_CASILLA, fila * TAM_CASILLA,
+                (columna + 1) * TAM_CASILLA, (fila + 1) * TAM_CASILLA);
+        }
+    }
 
-	//Registrar los callbacks
-	glutDisplayFunc(OnDraw);
-	glutTimerFunc(25, OnTimer, 0);//le decimos que dentro de 25ms llame 1 vez a la funcion OnTimer()
-	glutKeyboardFunc(OnKeyboardDown);
-	glutSpecialFunc(onSpecialKeyboardDown); //gestion de los cursores
-	glutMouseFunc(ControlRaton); //gestion de raton
-
-	// crear menus
-	createMenus();
-
-	//pasarle el control a GLUT,que llamara a los callbacks
-	glutMainLoop();
-
-	return 0;
+    glFlush();
 }
 
-void OnDraw(void)
-{
-	//Borrado de la pantalla	
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+int main(int argc,char* argv[]) {
+	
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(TAM_CASILLA* TAM_TABLERO, TAM_CASILLA * TAM_TABLERO);
+    glutCreateWindow("Ajedrez");
 
-	//Para definir el punto de vista
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(0, TAM_CASILLA * TAM_TABLERO + 75, TAM_CASILLA * TAM_TABLERO + 75, 0, -1, 1);
 
-	ajedrez.dibuja();
+    glutDisplayFunc(display);
 
-	//no borrar esta linea ni poner nada despues
-	glutSwapBuffers();
-}
+    glutMainLoop();
 
-void onSpecialKeyboardDown(int key, int x, int y)
-{
-	ajedrez.teclaEspecial(key);
-	glutPostRedisplay();
-}
-
-void OnTimer(int value)
-{
-	//poner aqui el código de animacion
-	ajedrez.mueve();
-
-	//no borrar estas lineas
-	glutTimerFunc(25, OnTimer, 0);
-	glutPostRedisplay();
-}
-void ControlRaton(int button, int state, int x, int y)
-{
-	float w = glutGet(GLUT_WINDOW_WIDTH);
-	float h = glutGet(GLUT_WINDOW_HEIGHT);
-	if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
-		Casilla casilla;
-		casilla.x = 10 * (x / w - 0.1);
-		casilla.y = 10 * ((1 - y / h) - 0.1);
-		std::cout << "Casilla:" << casilla.x << "," << casilla.y << endl;
-		ajedrez.raton(casilla);
-	}
-
-}
-void processMenuEvents(int value)
-{
-	ajedrez.processMenuEvents((unsigned char)value);
-}
-
-void createMenus() {
-
-	// create the menu and
-	// tell glut that "processMenuEvents" will
-	// handle the events
-	int menu1 = glutCreateMenu(processMenuEvents);
-
-	//add entries to our menu
-	glutAddMenuEntry("Iniciar Partida", 'e');
-	glutAddMenuEntry("Salir del Juego", 's');
-
-	if (ajedrez.getEstado() == 1) // 1 = JUEGO
-	{
-		int menu2 = glutCreateMenu(processMenuEvents);
-
-		glutAddMenuEntry("Reiniciar partida", 'r');
-		glutAddMenuEntry("Salir del Juego", 's');
-	}
-	// attach the menu to the right button
-	glutAttachMenu(GLUT_RIGHT_BUTTON);
+    return 0;
 }

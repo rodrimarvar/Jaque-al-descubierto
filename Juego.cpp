9e3382e5fab25b60bd9fc2776b3737b4ae3184f6
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-04-30 11:35:29 +0200

    Update Juego.cpp

--- Juego.cpp
+++ Juego.cpp
@@ -1,6 +1,9 @@
 #include "freeglut.h"
 #include "Coordenada.h"
 #include "Tablero.h"
+#include <iostream>
+
+using namespace std;
 
 const int TAM_CASILLA = 75;
 
@@ -12,6 +15,23 @@ void display() {
     // Borde del tablero
     glColor3f(0.4f, 0.2f, 0.0f);
     glRecti(-75, -75, TAM_CASILLA * 10, TAM_CASILLA * 10);
+	
+    // Dibujar posiciones del tablero
+    glColor3f(1.0f, 1.0f, 1.0f);
+
+    for (int i = 0; i < TAM_TABLERO; i++) {
+
+        char letra = 'a';
+
+        for (int j = 0; j < 2; j = j++) {
+            
+            glRasterPos2i((i + 1) * TAM_CASILLA + TAM_CASILLA / 2, j*TAM_CASILLA*(TAM_TABLERO + 1) + TAM_CASILLA / 2);
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, letra + i);
+            glRasterPos2i(TAM_CASILLA / 2 + j * TAM_CASILLA * (TAM_TABLERO + 1), (i + 1) * TAM_CASILLA + TAM_CASILLA / 2); 
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '8' - i);
+        }
+    }
+
 
     // Tablero
     for (int fila = 1; fila < TAM_TABLERO; fila++) {

aa3452fcbe4cab7cfe2f86e108896c1833941cea
Eugenio-Fernandez <80359021+Eugenio-Fernandez@users.noreply.github.com>
2023-05-05 14:21:59 +0200

    Update Tablero.h

--- src/Tablero.h
+++ src/Tablero.h
@@ -9,8 +9,16 @@ class Tablero {
 public:
     Tablero();
     ~Tablero();
-
-
+    //saber posición de la pieza
+    Pieza* obtenerPosicionPieza(Coordenada posicion);
+    // colocar la pieza en una posición determinada (al inicio)
+    void colocarPieza(Coordenada posicion, Pieza* pieza);
+    //mover las piezas de ua coordenada a otra
+    bool moverPieza(Coordenada origen, Coordenada destino);
+    //comprobar que la casilla está dentro del tablero
+    bool esCoordenadaValida(Coordenada coordenada) const;
+    
 private:
     Pieza* casillas[TAM_TABLERO][TAM_TABLERO];
 };
+
